119806f2e5917de33b46afc4571ebdc6dff6af5d
xb21 <779319955@qq.com>
2018-03-20 16:20:31 +0800

    3

---
+++ 3/ball.js
@@ -0,0 +1,35 @@
+
+var Ball = function(game) {
+    var o = game.imageByName('ball')
+    // var image = imageFromPath('ball.png')
+    o.x = 100
+    o.y = 200
+    o.speedX = 5
+    o.speedY = 5
+    o.fired = false
+
+    o.fire = function() {
+        o.fired = true
+    }
+    // 动一次
+    // 球碰到墙壁反弹
+    o.move = function() {
+        if (o.fired) {
+            // log('move')
+            if (o.x < 0 || o.x > 400) {
+                o.speedX = -o.speedX
+            }
+            if (o.y < 0 || o.y > 300) {
+                o.speedY = -o.speedY
+            }
+            // move
+            o.x += o.speedX
+            o.y += o.speedY
+        }
+    }
+    // 垂直方向反弹
+    o.反弹 = function() {
+        o.speedY *= -1
+    }
+    return o
+}
